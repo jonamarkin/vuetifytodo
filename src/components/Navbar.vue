9f9a4c6d0fe035afc718fa011991b5c48a060ff8
jonamarkin <jona10711@gmail.com>
2019-09-19 12:15:36 -0400

    Dashboard design

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,30 @@
 <template>
     <nav>
-        <v-toolbar flat app>
-            <v-app-bar-nav-icon color="grey--text" @click="drawer =!drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title class="grey--text">
-                <span class="font-weight-light">VUETIFY</span>
-                <span>TODO</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn flat color="grey lighten-1">
-                <span>Sign Out</span>
-                <v-icon right>exit_to_app</v-icon>
-            </v-btn>
-        </v-toolbar>
-
-        <v-navigation-drawer v-model="drawer" color="purple lighten-2" temporary absolute>
-            <v-list>
-                <v-list-item-group v-model="link">
-                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+        <v-navigation-drawer v-model="drawer" app color="purple lighten-2">
+            <v-list dense>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-icon>
                         <v-icon v-text="link.icon" class="white--text"></v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title v-text="link.text" class="white--text"></v-list-item-title>
                     </v-list-item-content>
-                    </v-list-item>
-                </v-list-item-group>
+                </v-list-item>
             </v-list>
         </v-navigation-drawer>
 
+        <v-app-bar app flat color="grey--text">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="grey--text">
+                <span class="font-weight-light">PROJECT</span>
+                <span>TRACKER</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey lighten-1">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
     </nav>
 </template>
 
---
+++ src/views/Team.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="subheading grey--text">Team</h1>
+
+        <v-container fluid class="my-5">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, ab quibusdam aut sit praesentium esse accusantium totam voluptate ipsa. Consequuntur laboriosam dolore aliquid voluptatum veritatis pariatur vel a debitis non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque reprehenderit, eveniet, porro labore, esse incidunt magni vero odit deserunt iusto modi quibusdam laboriosam! Facilis fugiat non sed cum, odit eius.</p>
+
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
