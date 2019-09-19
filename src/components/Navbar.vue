3eebacdd38a80f1adb74bfbdcbce5c720017c389
jonamarkin <jona10711@gmail.com>
2019-09-19 07:08:19 -0400

    Navbar done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon color="grey--text" @click="drawer =!drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="grey--text">
+                <span class="font-weight-light">VUETIFY</span>
+                <span>TODO</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey lighten-1">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" class="indigo" temporary absolute>
+            <p>test</p>
+        </v-navigation-drawer>
+
+    </nav>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            drawer:false
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is the homepage</h1>
+    <v-btn class="pink white--text">Click Me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon>email</v-icon>
+    </v-btn>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
 
 export default {
-  components: {
-    HelloWorld,
-  },
+
 };
 </script>
