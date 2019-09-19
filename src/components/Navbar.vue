f138e851b033130c0b29d1021a687b0e9d3c44e6
jonamarkin <jona10711@gmail.com>
2019-09-19 07:58:52 -0400

    Sidebar done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,8 +13,19 @@
             </v-btn>
         </v-toolbar>
 
-        <v-navigation-drawer v-model="drawer" class="indigo" temporary absolute>
-            <p>test</p>
+        <v-navigation-drawer v-model="drawer" color="purple lighten-2" temporary absolute>
+            <v-list>
+                <v-list-item-group v-model="link">
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-icon>
+                        <v-icon v-text="link.icon" class="white--text"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="link.text" class="white--text"></v-list-item-title>
+                    </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
         </v-navigation-drawer>
 
     </nav>
@@ -25,7 +36,12 @@
 export default {
     data(){
         return{
-            drawer:false
+            drawer:false,
+            links:[
+                {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'folder', text: 'My Projects', route: '/projects'},
+                {icon: 'person', text: 'Team', route: '/team'}
+            ]
         }
     }
 }
