a1d60d6572e4dac9befb4328b5c6cb0e5247b279
jonamarkin <jona10711@gmail.com>
2019-09-20 13:20:17 -0400

    Dialogs added with datepicker7

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,12 +11,20 @@
           <h3>Add A New Project</h3>
         </v-card-title>
 
-        <v-card-text>
-          <v-form class="px-3">
-              <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-              <v-textarea label="Information" prepend-icon="edit" v-model="content"></v-textarea>
-              <v-btn class="success mx-0 mt-3" @click="submit">ADD PROJECT</v-btn>
-          </v-form>
+            <v-card-text>
+            <v-form class="px-3">
+                <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+                <v-textarea label="Information" prepend-icon="edit" v-model="content"></v-textarea>
+                    
+                    <v-menu>
+                        <template v-slot:activator="{ on }">
+                            <v-text-field :value="formatteddate" clearable label="Due Date" readonly v-on="on" prepend-icon="date_range"></v-text-field>
+                        </template>
+                        <v-date-picker v-model="formatteddate"></v-date-picker>
+                    </v-menu>
+                    
+                <v-btn class="success mx-0 mt-3" @click="submit">ADD PROJECT</v-btn>
+            </v-form>
         </v-card-text>
 
         <v-card-actions>
@@ -27,16 +35,26 @@
 </template>
 
 <script>
+
+import format from 'date-fns/format'
+
 export default {
     data(){
         return{
             title: "",
-            content: ""
+            content: "",
+            date:null,
+            formatteddate: null
         }
     },
     methods:{
         submit(){
-            console.log(this.title, this.content)
+            //console.log(this.title, this.content)
+        }
+    },
+    computed:{
+        formattedDate(){
+            this.formatteddate = format(this.date, 'DD MM YYYY')
         }
     }
 }
