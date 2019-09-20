9f86458b6d595ff822e179a7d975eb80a2aab7a3
jonamarkin <jona10711@gmail.com>
2019-09-20 11:49:57 -0400

    Dialog added

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,21 +1,23 @@
 <template>
     <v-dialog v-model="dialog" width="500 ">
       <template v-slot:activator="{ on }">
-        <v-btn color="red lighten-2" dark v-on="on">
+        <v-btn color="green lighten-2" dark v-on="on">
           Add A New Project
         </v-btn>
       </template>
 
       <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title  >
-          <h2>Add A New Project</h2>
+        <v-card-title class="headline" >
+          <h3>Add A New Project</h3>
         </v-card-title>
 
         <v-card-text>
-          
+          <v-form class="px-3">
+              <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+              <v-textarea label="Information" prepend-icon="edit" v-model="content"></v-textarea>
+              <v-btn class="success mx-0 mt-3" @click="submit">ADD PROJECT</v-btn>
+          </v-form>
         </v-card-text>
-
-        <v-divider></v-divider>
 
         <v-card-actions>
           
@@ -26,6 +28,16 @@
 
 <script>
 export default {
-    
+    data(){
+        return{
+            title: "",
+            content: ""
+        }
+    },
+    methods:{
+        submit(){
+            console.log(this.title, this.content)
+        }
+    }
 }
 </script>
